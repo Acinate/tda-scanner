59f8d1d9aa8321824eb346898a6c1824a676842d
Acinate <Acinate@hotmail.com>
2019-10-06 19:30:10 -0400

    refactored nyse scanner

--- src/main/java/com/scanner/symbols/NYSE/NYSEScanner.java
+++ src/main/java/com/scanner/symbols/NYSE/NYSEScanner.java
@@ -1,6 +1,7 @@
 package com.scanner.symbols.NYSE;
 
 import com.google.gson.Gson;
+import com.models.Symbol;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
@@ -8,15 +9,76 @@ import kong.unirest.json.JSONArray;
 import kong.unirest.json.JSONObject;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class NYSEScanner {
 
-    Gson gson = new Gson();
-    String url = "https://www.nyse.com/api/quotes/filter";
-    String cookie = "__cfduid=db19a0ddf9582bcdef1f6be3737b485261567429706; _ga=GA1.2.194448053.1567429708; ICE_notification=462751242.56425.0000; _gid=GA1.2.1259522087.1570200589; cookieDisclaimer=true; JSESSIONID=3F718C6C5585F83454EA0F34A82B14CE; _gat_UA-97108014-2=1; ICE=!T2B1ePBlCW/iprA+xkwKbNFUCBVLalFX+MMyuO6Wf+QIZPe0r7jrhOC5g0vMsLPmNVPfmHOmtOcyRg==; TS01ebd031=0100e6d49590e0af3beef5f49cb7629d623ed0c762f64010911cc51daf0d33dfa9ee93f618db82ac70333c16a79fe444b5cfea96b519cb3787ec1b335070360a3c515d5471e7723e493b07408d3aec21021ef20687316ee069fe56eb8bd41bc77a98f692d2";
+    public HashMap<String, Symbol> GetAllSymbols() {
+        return GetPages("");
+    }
 
-    /* assetType = {"EQUITY", "EXCHANGE_TRADED_FUND", "INDEX"} */
+    public HashMap<String, Symbol> GetStockSymbols() {
+        return GetPages("stock");
+    }
+
+    public HashMap<String, Symbol> GetIndexSymbols() {
+        return GetPages("index");
+    }
+
+    public HashMap<String, Symbol> GetEtfSymbols() {
+        return GetPages("etf");
+    }
+
+    private HashMap<String, Symbol> GetPages(String asset) {
+        HashMap<String, Symbol> symbols = new HashMap<>();
+        int count = GetCount(asset);
+        int page = 1;
+        int pageSize = 10;
+        for (page = 1; page < (count / pageSize); page++) {
+            symbols.putAll(GetPage(asset, page, pageSize));
+        }
+        return symbols;
+    }
+
+    private HashMap<String, Symbol> GetPage(String asset, int page, int pageSize) {
+        String url = "https://www.nyse.com/api/quotes/filter";
+        HashMap<String, Symbol> symbols = new HashMap<>();
+        HttpResponse<JsonNode> response = Unirest.post(url)
+                .body(GetBody(GetAssetType(asset), page, pageSize))
+                .headers(GetHeaders())
+                .asJson();
+
+        if (response.isSuccess()) {
+            JSONArray rows = response.getBody().getArray();
+            for (Object row : rows) {
+                String ticker = ((JSONObject) row).getString("symbolTicker");
+                String instrumentType = ((JSONObject) row).getString("instrumentType");
+                String assetType = ParseInstrumentType(instrumentType);
+                String exchange = "NYSE";
+                Symbol symbol = new Symbol(ticker, exchange, ParseInstrumentType(assetType));
+                symbols.put(symbol.getTicker(), symbol);
+            }
+        }
+        return symbols;
+    }
+
+    private int GetCount(String asset) {
+        String url = "https://www.nyse.com/api/quotes/filter";
+        String assetType = GetAssetType(asset);
+        HttpResponse<JsonNode> response = Unirest.post(url)
+                .body(GetBody(assetType, 1, 1))
+                .headers(GetHeaders())
+                .asJson();
+        if (response.isSuccess()) {
+            if (response.getBody().getArray().length() > 0) {
+                return ((JSONObject) response.getBody().getArray().get(0)).getInt("total");
+            }
+        }
+        return -1;
+    }
+
     private String GetBody(String assetType, int page, int pageSize) {
+        Gson gson = new Gson();
         NYSEBody body = new NYSEBody();
 
         body.instrumentType = assetType;
@@ -29,109 +91,46 @@ public class NYSEScanner {
         return gson.toJson(body);
     }
 
-    private int GetStockCount() {
-        HttpResponse<JsonNode> response = Unirest.post(url)
-                .body(GetBody("EQUITY",1, 10))
-                .header("Cookie", cookie)
-                .header("Content-Type", "application/json")
-                .asJson();
-
-        JSONObject stock = (JSONObject) response.getBody().getArray().get(0);
-
-        return stock.getInt("total");
-    }
-
-    public HashMap<String, NYSEStock> GetStocks() {
-        int count = GetStockCount();
-        int pageSize = 10;
-        int page = 1;
-
-        HashMap<String, NYSEStock> stocks = new HashMap<>();
-        for (page = 1; page <= (count / pageSize + 1); page++) {
-            HttpResponse<JsonNode> response = Unirest.post(url)
-                    .body(GetBody("EQUITY",page,pageSize))
-                    .header("Cookie", cookie)
-                    .header("Content-Type", "application/json")
-                    .asJson();
-
-            JSONArray results = response.getBody().getArray();
-
-            for (Object result : results) {
-                NYSEStock s = new NYSEStock((JSONObject) result);
-                stocks.put(s.getTicker(), s);
-            }
-        System.out.println(page);
+    private String GetAssetType(String asset) {
+        switch (asset) {
+            case "stock":
+                return "EQUITY";
+            case "etf":
+                return "EXCHANGE_TRADED_FUND";
+            case "index":
+                return "INDEX";
+            default:
+                return "INVALID_ASSET_TYPE";
         }
-        return stocks;
     }
 
-    private int GetETFCount() {
-        HttpResponse<JsonNode> response = Unirest.post(url)
-                .body(GetBody("EXCHANGE_TRADED_FUND",1, 10))
-                .header("Cookie", cookie)
-                .header("Content-Type", "application/json")
-                .asJson();
-
-        JSONObject stock = (JSONObject) response.getBody().getArray().get(0);
-
-        return stock.getInt("total");
-    }
-    public HashMap<String, NYSEStock> GetETFs() {
-        int count = GetETFCount();
-        int pageSize = 10;
-        int page = 1;
-
-        HashMap<String, NYSEStock> stocks = new HashMap<>();
-        for (page = 1; page <= (count / pageSize + 1); page++) {
-            HttpResponse<JsonNode> response = Unirest.post(url)
-                    .body(GetBody("EXCHANGE_TRADED_FUND",page,pageSize))
-                    .header("Cookie", cookie)
-                    .header("Content-Type", "application/json")
-                    .asJson();
-
-            JSONArray results = response.getBody().getArray();
-
-            for (Object result : results) {
-                NYSEStock s = new NYSEStock((JSONObject) result);
-                stocks.put(s.getTicker(), s);
-            }
-            System.out.println(page);
+    private String ParseInstrumentType(String instrument) {
+        switch (instrument) {
+            case "COMMON_STOCK":
+                return "Stock";
+            case "INDEX":
+                return "Index";
+            case "CONVERTIBLE_BOND":
+                return "Bond";
+            case "PREFERRED_STOCK":
+                return "Preferred Stock";
+            case "EXCHANGE_TRADED_FUND":
+                return "ETF";
+            default:
+                return instrument;
         }
-        return stocks;
     }
 
-    private int GetIndexCount() {
-        HttpResponse<JsonNode> response = Unirest.post(url)
-                .body(GetBody("INDEX",1, 10))
-                .header("Cookie", cookie)
-                .header("Content-Type", "application/json")
-                .asJson();
-
-        JSONObject stock = (JSONObject) response.getBody().getArray().get(0);
-
-        return stock.getInt("total");
+    private Map<String, String> GetHeaders() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Cookie", GetCookie());
+        headers.put("Content-Type", "application/json");
+        return headers;
     }
-    public HashMap<String, NYSEStock> GetIndexes() {
-        int count = GetIndexCount();
-        int pageSize = 10;
-        int page = 1;
 
-        HashMap<String, NYSEStock> stocks = new HashMap<>();
-        for (page = 1; page <= (count / pageSize + 1); page++) {
-            HttpResponse<JsonNode> response = Unirest.post(url)
-                    .body(GetBody("INDEX",page,pageSize))
-                    .header("Cookie", cookie)
-                    .header("Content-Type", "application/json")
-                    .asJson();
-
-            JSONArray results = response.getBody().getArray();
-
-            for (Object result : results) {
-                NYSEStock s = new NYSEStock((JSONObject) result);
-                stocks.put(s.getTicker(), s);
-            }
-            System.out.println(page);
-        }
-        return stocks;
+    private String GetCookie() {
+        return "__cfduid=db19a0ddf9582bcdef1f6be3737b485261567429706; _ga=GA1.2.194448053.1567429708; " +
+                "ICE_notification=462751242.56425.0000; _gid=GA1.2.1259522087.1570200589; cookieDisclaimer=true; " +
+                "JSESSIONID=3F718C6C5585F83454EA0F34A82B14CE; _gat_UA-97108014-2=1; ICE=!T2B1ePBlCW/iprA+xkwKbNFUCBVLalFX+MMyuO6Wf+QIZPe0r7jrhOC5g0vMsLPmNVPfmHOmtOcyRg==; TS01ebd031=0100e6d49590e0af3beef5f49cb7629d623ed0c762f64010911cc51daf0d33dfa9ee93f618db82ac70333c16a79fe444b5cfea96b519cb3787ec1b335070360a3c515d5471e7723e493b07408d3aec21021ef20687316ee069fe56eb8bd41bc77a98f692d2";
     }
 }
