c5fecdbf7c310733e099b053275560a7eaadfe30
Acinate <Acinate@hotmail.com>
2019-10-06 19:30:33 -0400

    increased nyse scanner test coverage

--- src/test/java/scanner/symbols/NYSE/TestNYSEScanner.java
+++ src/test/java/scanner/symbols/NYSE/TestNYSEScanner.java
@@ -1,24 +1,81 @@
 package scanner.symbols.NYSE;
 
+import com.models.Symbol;
 import com.scanner.symbols.NYSE.NYSEScanner;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class TestNYSEScanner {
+import java.lang.reflect.Method;
+import java.util.HashMap;
 
+class TestNYSEScanner {
     private NYSEScanner nyseScanner = new NYSEScanner();
 
     @Test
-    void TestNyseStockScanner() {
-        nyseScanner.GetStocks();
+    void TestGetCount_Stock() throws Exception {
+        Method method = NYSEScanner.class.getDeclaredMethod("GetCount", String.class);
+        method.setAccessible(true);
+        int count = (int) method.invoke(nyseScanner, "stock");
+        Assertions.assertTrue(count >= 0);
     }
 
     @Test
-    void TestNyseEtfScanner() {
-        nyseScanner.GetETFs();
+    void TestGetCount_Index() throws Exception {
+        Method method = NYSEScanner.class.getDeclaredMethod("GetCount", String.class);
+        method.setAccessible(true);
+        int count = (int) method.invoke(nyseScanner, "index");
+        Assertions.assertTrue(count >= 0);
     }
 
     @Test
-    void TestNyseIndexScanner() {
-        nyseScanner.GetIndexes();
+    void TestGetCount_Etf() throws Exception {
+        Method method = NYSEScanner.class.getDeclaredMethod("GetCount", String.class);
+        method.setAccessible(true);
+        int count = (int) method.invoke(nyseScanner, "etf");
+        Assertions.assertTrue(count >= 0);
+    }
+
+    @Test
+    void TestGetCount_Invalid() throws Exception {
+        Method method = NYSEScanner.class.getDeclaredMethod("GetCount", String.class);
+        method.setAccessible(true);
+        int count = (int) method.invoke(nyseScanner, "invalidAssetType");
+        Assertions.assertEquals(-1, count);
+    }
+
+    @Test
+    void TestGetPage_Stock() throws Exception {
+        Method method = NYSEScanner.class.getDeclaredMethod("GetPage", String.class, int.class, int.class);
+        method.setAccessible(true);
+        HashMap<String, Symbol> symbols = (HashMap<String, Symbol>) method.invoke(nyseScanner, "stock", 1, 10);
+        Assertions.assertNotNull(symbols);
+        Assertions.assertEquals(10, symbols.size());
+    }
+
+    @Test
+    void TestGetPage_Index() throws Exception {
+        Method method = NYSEScanner.class.getDeclaredMethod("GetPage", String.class, int.class, int.class);
+        method.setAccessible(true);
+        HashMap<String, Symbol> symbols = (HashMap<String, Symbol>) method.invoke(nyseScanner, "index", 1, 10);
+        Assertions.assertNotNull(symbols);
+        Assertions.assertEquals(10, symbols.size());
+    }
+
+    @Test
+    void TestGetPage_Etf() throws Exception {
+        Method method = NYSEScanner.class.getDeclaredMethod("GetPage", String.class, int.class, int.class);
+        method.setAccessible(true);
+        HashMap<String, Symbol> symbols = (HashMap<String, Symbol>) method.invoke(nyseScanner, "etf", 1, 10);
+        Assertions.assertNotNull(symbols);
+        Assertions.assertEquals(10, symbols.size());
+    }
+
+    @Test
+    void TestGetPage_Invalid() throws Exception {
+        Method method = NYSEScanner.class.getDeclaredMethod("GetPage", String.class, int.class, int.class);
+        method.setAccessible(true);
+        HashMap<String, Symbol> symbols = (HashMap<String, Symbol>) method.invoke(nyseScanner, "invalidAsset", 1, 10);
+        Assertions.assertNotNull(symbols);
+        Assertions.assertEquals(0, symbols.size());
     }
 }
